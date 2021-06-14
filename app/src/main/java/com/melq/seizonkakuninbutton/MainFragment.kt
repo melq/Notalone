8110b3e82308b0b2808a5f3af7773906c8d4eeec
melq <tu2sice@yahoo.co.jp>
2021-06-14 15:07:02 +0900

    :sparkles: Splash画面を表示するようにした

--- app/src/main/java/com/melq/seizonkakuninbutton/MainFragment.kt
+++ app/src/main/java/com/melq/seizonkakuninbutton/MainFragment.kt
@@ -1,8 +1,17 @@
 package com.melq.seizonkakuninbutton
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
 
 class MainFragment : Fragment(R.layout.fragment_main) {
     private val vm: MainViewModel by viewModels()
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        findNavController().navigate(R.id.action_mainFragment_to_loginFragment)
+    }
 }
--- app/src/main/java/com/melq/seizonkakuninbutton/page/splash/SplashFragment.kt
+++ app/src/main/java/com/melq/seizonkakuninbutton/page/splash/SplashFragment.kt
@@ -1,7 +1,22 @@
 package com.melq.seizonkakuninbutton.page.splash
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.melq.seizonkakuninbutton.R
+import com.melq.seizonkakuninbutton.databinding.FragmentSplashBinding
 
 class SplashFragment : Fragment(R.layout.fragment_splash) {
+    private var _binding: FragmentSplashBinding? = null
+    private val binding: FragmentSplashBinding get() = _binding!!
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        this._binding = FragmentSplashBinding.bind(view)
+
+        binding.constraintLayout.setOnClickListener {
+            findNavController().navigate(R.id.action_splashFragment_to_mainFragment)
+        }
+    }
 }
