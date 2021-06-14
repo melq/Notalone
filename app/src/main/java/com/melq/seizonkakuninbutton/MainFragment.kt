99462263ed65c279bf2eb6918728b2b78ea6af0e
melq <tu2sice@yahoo.co.jp>
2021-06-14 15:51:10 +0900

    :art: main画面を作成

--- app/src/main/java/com/melq/seizonkakuninbutton/MainFragment.kt
+++ app/src/main/java/com/melq/seizonkakuninbutton/MainFragment.kt
@@ -5,13 +5,20 @@ import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
+import com.melq.seizonkakuninbutton.databinding.FragmentMainBinding
 
 class MainFragment : Fragment(R.layout.fragment_main) {
     private val vm: MainViewModel by viewModels()
 
+    private var _binding: FragmentMainBinding? = null
+    private val binding: FragmentMainBinding get() = _binding!!
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        _binding = FragmentMainBinding.bind(view)
 
-        findNavController().navigate(R.id.action_mainFragment_to_loginFragment)
+        binding.btMain.setOnClickListener {
+            findNavController().navigate(R.id.action_mainFragment_to_loginFragment)
+        }
     }
 }
