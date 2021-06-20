e4516167c7fc63696bf26f6fe6eb1e926eee500c
melq <tu2sice@yahoo.co.jp>
2021-06-20 14:23:13 +0900

    :construction: createUserのベースを作成

--- app/src/main/java/com/melq/seizonkakuninbutton/model/data/DataRepository.kt
+++ app/src/main/java/com/melq/seizonkakuninbutton/model/data/DataRepository.kt
@@ -1,5 +1,6 @@
 package com.melq.seizonkakuninbutton.model.data
 
+import android.util.Log
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
 
@@ -8,4 +9,20 @@ class DataRepository {
         const val collectionName = "data"
     }
     private val db = Firebase.firestore
+
+    fun createUser(id: String) {
+        val tag = "CREATE_USER"
+        val doc = db.collection(collectionName).document(id)
+        doc.get()
+            .addOnSuccessListener { document ->
+                if (document.data != null) {
+                    Log.d(tag, "DocumentSnapshot exists data: ${document.data}")
+                } else {
+                    Log.d(tag, "No such document")
+                }
+            }
+            .addOnFailureListener { e ->
+                Log.w(tag, "create failed with", e)
+            }
+    }
 }
