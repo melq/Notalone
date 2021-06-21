6dc27cc68863b0eb678c9c8d81311326fe3dbb6a
melq <tu2sice@yahoo.co.jp>
2021-06-21 15:17:26 +0900

    :sparkles: splash画面から自動で遷移するようにした

--- app/src/main/java/com/melq/seizonkakuninbutton/page/splash/SplashFragment.kt
+++ app/src/main/java/com/melq/seizonkakuninbutton/page/splash/SplashFragment.kt
@@ -1,6 +1,7 @@
 package com.melq.seizonkakuninbutton.page.splash
 
 import android.os.Bundle
+import android.os.Handler
 import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
@@ -11,12 +12,23 @@ class SplashFragment : Fragment(R.layout.fragment_splash) {
     private var _binding: FragmentSplashBinding? = null
     private val binding: FragmentSplashBinding get() = _binding!!
 
+    private val handler = Handler()
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         this._binding = FragmentSplashBinding.bind(view)
 
-        binding.constraintLayout.setOnClickListener {
+        val runnable = Runnable {
             findNavController().navigate(R.id.action_splashFragment_to_mainFragment)
         }
+        handler.postDelayed(
+            runnable,
+            2000
+        )
+
+        /*binding.constraintLayout.setOnClickListener {
+            handler.removeCallbacksAndMessages(null)
+            findNavController().navigate(R.id.action_splashFragment_to_mainFragment)
+        }*/
     }
 }
