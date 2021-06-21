d00c747bf14c9ac96588b89e6256a342ad89a695
melq <tu2sice@yahoo.co.jp>
2021-06-21 15:32:26 +0900

    :art: メイン画面からsplash画面にバックキーで戻れないようにした

--- app/src/main/java/com/melq/seizonkakuninbutton/MainFragment.kt
+++ app/src/main/java/com/melq/seizonkakuninbutton/MainFragment.kt
@@ -2,6 +2,7 @@ package com.melq.seizonkakuninbutton
 
 import android.os.Bundle
 import android.view.View
+import androidx.activity.addCallback
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
@@ -12,6 +13,12 @@ class MainFragment : Fragment(R.layout.fragment_main) {
 
     private var _binding: FragmentMainBinding? = null
     private val binding: FragmentMainBinding get() = _binding!!
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        requireActivity().onBackPressedDispatcher.addCallback(this) {  }
+    }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
