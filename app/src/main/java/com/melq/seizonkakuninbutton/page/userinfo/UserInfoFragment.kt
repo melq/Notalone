a9246c8df2f4cfdc149d0eeac715271b9e4b8b2c
melq <tu2sice@yahoo.co.jp>
2021-06-28 15:43:55 +0900

    :art: ログアウト時に確認ダイアログを出すようにした

--- app/src/main/java/com/melq/seizonkakuninbutton/page/userinfo/UserInfoFragment.kt
+++ app/src/main/java/com/melq/seizonkakuninbutton/page/userinfo/UserInfoFragment.kt
@@ -1,5 +1,6 @@
 package com.melq.seizonkakuninbutton.page.userinfo
 
+import android.app.AlertDialog
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
@@ -9,6 +10,7 @@ import com.google.android.material.snackbar.Snackbar
 import com.melq.seizonkakuninbutton.MainViewModel
 import com.melq.seizonkakuninbutton.R
 import com.melq.seizonkakuninbutton.databinding.FragmentUserInfoBinding
+import kotlinx.coroutines.NonCancellable.cancel
 
 class UserInfoFragment : Fragment(R.layout.fragment_user_info) {
     private val vm: MainViewModel by activityViewModels()
@@ -37,13 +39,20 @@ class UserInfoFragment : Fragment(R.layout.fragment_user_info) {
         }
 
         binding.btLogout.setOnClickListener {
-            vm.logoutClicked()
-            vm.done.observe(viewLifecycleOwner) {
-                if (it == true) {
-                    findNavController().popBackStack()
-                    vm.done.value = false
+            androidx.appcompat.app.AlertDialog.Builder(requireContext())
+                .setTitle(R.string.logout)
+                .setMessage(R.string.ask_logout)
+                .setPositiveButton(R.string.ok) { _, _ ->
+                    vm.logoutClicked()
+                    vm.done.observe(viewLifecycleOwner) {
+                        if (it == true) {
+                            findNavController().popBackStack()
+                            vm.done.value = false
+                        }
+                    }
                 }
-            }
+                .setNegativeButton(R.string.cancel) { _, _ -> }
+                .show()
         }
 
         // FEATURE: パスワード変更機能を付ける
