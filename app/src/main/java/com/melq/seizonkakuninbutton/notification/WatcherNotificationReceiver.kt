928977383a56d448e5103d1a432bf8a4204a47f8
melq <tu2sice@yahoo.co.jp>
2021-07-04 14:12:26 +0900

    :star: Watcher用通知のレシーバーを作成

---
+++ app/src/main/java/com/melq/seizonkakuninbutton/notification/WatcherNotificationReceiver.kt
@@ -0,0 +1,16 @@
+package com.melq.seizonkakuninbutton.notification
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+
+class WatcherNotificationReceiver : BroadcastReceiver() {
+    companion object {
+        fun setNotification() {
+
+        }
+    }
+
+    override fun onReceive(context: Context?, intent: Intent?) {
+    }
+}
