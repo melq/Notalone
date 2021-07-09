2fc9fd06d025dd92a70239208332e91e8acabda8
melq <tu2sice@yahoo.co.jp>
2021-07-09 23:20:59 +0900

    :sparkles: みまもり履歴のリスト画面からの遷移を実装した

--- app/src/main/java/com/melq/notalone/page/history/WatcherHistoryFragment.kt
+++ app/src/main/java/com/melq/notalone/page/history/WatcherHistoryFragment.kt
@@ -2,8 +2,10 @@ package com.melq.notalone.page.history
 
 import android.os.Bundle
 import android.util.Log
+import android.view.Menu
 import android.view.View
 import androidx.activity.addCallback
+import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.content.res.AppCompatResources
 import androidx.core.app.NotificationManagerCompat
 import androidx.fragment.app.Fragment
@@ -15,6 +17,7 @@ import com.google.firebase.Timestamp
 import com.melq.notalone.MainViewModel
 import com.melq.notalone.R
 import com.melq.notalone.databinding.FragmentWatcherHistoryBinding
+import com.melq.notalone.model.user.User
 import com.melq.notalone.notification.WatcherNotificationReceiver
 
 class WatcherHistoryFragment : Fragment(R.layout.fragment_watcher_history) {
@@ -43,6 +46,51 @@ class WatcherHistoryFragment : Fragment(R.layout.fragment_watcher_history) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         _binding = FragmentWatcherHistoryBinding.bind(view)
+
+        val toggle = ActionBarDrawerToggle(
+            requireActivity(),
+            binding.drawerLayout,
+            binding.toolbar,
+            R.string.app_name,
+            R.string.app_title
+        )
+        binding.drawerLayout.addDrawerListener(toggle)
+        toggle.syncState()
+
+        val checkList = mutableListOf( // 仮データリスト
+            mapOf("name" to "jiro", "id" to "5aUA9tnrVTgj2hvIve2GsnKTB2n2"),
+            mapOf("name" to "saburo", "id" to "wJghyQbPx5abVwLdQ9GiZ4VSF9z1"),
+            mapOf("name" to "shiro", "id" to "jEBri4JlJmftZLzCTZ4rA50eZfB2")
+        )
+        val sub = binding.navigationView.menu.addSubMenu(Menu.NONE, Menu.NONE, 10, R.string.check_history)
+        for (i in checkList.indices) {
+            val item = sub.add(Menu.NONE, i + 1, i, "${checkList[i]["name"]}")
+            item.setIcon(R.drawable.ic_baseline_perm_identity_vector)
+        }
+        val addId = 100
+        val item = sub.add(Menu.NONE, addId, checkList.size + 1, R.string.add_user)
+        item.setIcon(R.drawable.ic_baseline_add_vector)
+
+        binding.navigationView.setNavigationItemSelectedListener { menuItem ->
+            when (menuItem.itemId) {
+                R.id.menu_push -> {
+                    Log.d("WATCHER", "push clicked${menuItem.order}")
+                    return@setNavigationItemSelectedListener true
+                }
+                addId -> {
+                    Log.d("WATCHER", "add clicked")
+                    return@setNavigationItemSelectedListener true
+                }
+                else -> {
+                    val index = menuItem.order
+                    Log.d("WATCHER", "${checkList[index]["name"]} clicked")
+                    vm.watchUser = User(checkList[index]["id"]!!, "", "", mutableListOf())
+                    findNavController().navigate(R.id.action_watcherHistoryFragment_to_mainFragment)
+                    findNavController().navigate(R.id.action_mainFragment_to_watcherHistoryFragment)
+                }
+            }
+            return@setNavigationItemSelectedListener true
+        }
 
         val historyList = vm.watchUser.pushHistory
         adapter = MyAdapter(historyList, requireContext())
